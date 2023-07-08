24660bfeab64cba845b2c4f124d8c69328289575
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-08 16:07:08 -0700

    Create my_each enumerable

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    array = []
+    (0...length).each do |index|
+      array << self[index]
+      yield(self[index])
+    end
+    array
+  end
 end
